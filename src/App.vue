b9ed1a766921a303291c94ea9bbe23357c4019d1
elshod <elshod5f@gmail.com>
2021-07-29 21:07:43 +0500

    hospital loyihani yaratildi. bootstrap boglandi

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,98 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <h1>Test Bootstrap</h1>
+      <div class="d-flex justify-content-between">
+        <b-button v-b-modal.modal-newbemor>Yangi bemor</b-button>
+        <b-form-select v-model="search"  class="mb-3" :options="searchOption"></b-form-select>
+      </div>
+      <b-table striped hover 
+        :fields="fields" 
+        :items="filteredPatients">
+        <template #cell(btn)="data">
+          <b-button @click='del(data.index)' variant='danger'>
+            <b-icon-person-x-fill></b-icon-person-x-fill>
+          </b-button>
+        </template>
+      </b-table>
+
+      <b-modal id="modal-newbemor" hide-footer title="Yangi bemor">
+        <b-form-input v-model="patient.name" class="mb-3" placeholder="Ismini yozing"></b-form-input>
+        <label for="example-datepicker">Tug'ilgan sana</label>
+        <b-form-datepicker id="example-datepicker" v-model="patient.bdate" class="mb-3"></b-form-datepicker>
+        <b-form-group label="Jinsni tanlang" v-slot="{ ariaDescribedby }" >
+          <div class="d-flex">
+            <div class="mr-3">
+              <b-form-radio v-model="patient.gender" :aria-describedby="ariaDescribedby" name="gender" class="mr-3" value="Erkak">Erkak</b-form-radio>
+            </div>
+            <div class="mr-3">
+              <b-form-radio v-model="patient.gender" :aria-describedby="ariaDescribedby" name="gender" class="ml-3" value="Ayol">Ayol</b-form-radio>
+            </div>
+          </div>
+        </b-form-group>
+        <label for="doctor" class="mt-3">Shifokorni tanlang</label>
+        <b-form-select v-model="patient.doctor" id="doctor" class="mb-3 form-control" :options="doctors"></b-form-select>
+        <label for="time">Qabul vaqtini tanlang</label>
+        <b-form-timepicker id="time" v-model="patient.time" locale="ru"></b-form-timepicker>
+        <b-button variant='success' class="mt-3" @click='add()'>Qo'shish</b-button>
+      </b-modal>
+    </div>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      patient:{},
+      doctors: ['Bahodirov Aziz','Yo`ldoshev Farhod','Olimova Aziza'],
+      patients: [],
+      search:'Barchasi',
+      searchOption: ['Barchasi','Erkak','Ayol'],
+      fields: [
+          { key: 'name', label: 'Ism' },
+          { key: 'bdate', label: 'Tugilgan sana'},
+          { key: 'gender', label: 'Jinsi'},
+          { key: 'doctor', label: 'Davolovchi shifokor'},
+          { key: 'time', label: 'Qabul vaqti'},
+          { key: 'btn', label: 'O`chirish'},
+        ],
+    }
+  },
+  methods: {
+    add(){
+      this.patients.push(this.patient)
+      this.patient = {}
+      localStorage.setItem('patients',JSON.stringify(this.patients))
+      this.$bvModal.hide('modal-newbemor')
+    },
+    del(index){
+      this.patients.splice(index,1)
+      localStorage.setItem('patients',JSON.stringify(this.patients))
+    }
+  },
+  computed: {
+    filteredPatients(){
+      if (this.search == 'Barchasi') return this.patients
+      return this.patients.filter(patient => {
+        return patient.gender == this.search
+      })
+    }
+  },
+  created(){
+    if (localStorage.getItem('patients')){
+      try {
+        this.patients = JSON.parse(localStorage.getItem('patients'))
+      } catch (error) {
+        localStorage.removeItem('patients')
+      }
+    }
   }
 }
 </script>
-
+ 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .custom-radio{
+    margin-right: 10px;
+  }
 </style>
